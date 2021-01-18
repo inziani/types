c64203ce4e73acdb7015788f40223bc0cc4305fb
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-01-18 18:02:46 +0300

    Project set up

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`)
+
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`)
+myVar = 'Hello';
+console.log(`${myVar} = ${typeof myVar}`)
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`)
+
+function calaculateTax(amount: any){
+  return amount * 0.16;
+
+}
+
+console.log(`${12} = ${calaculateTax(12)}`);
+console.log(`${'Hello'} = ${calaculateTax('Hello')}`);
+console.log(`${'True'} = ${calaculateTax(true)}`);
