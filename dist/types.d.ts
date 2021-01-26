4f07f1d8ae25821fbd0426066240e6dc69a8cf63
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-01-26 10:34:14 +0300

    functions

---
+++ dist/types.d.ts
@@ -0,0 +1,11 @@
+declare let myVar: any;
+declare function calaculateTax(amount: number): number;
+declare function calculateTaxInclusive(amount: any): any;
+declare let price: number;
+declare let taxAmount: number;
+declare let halfShare: string;
+declare function newCalculateTax(amount: number, format: boolean): string | number;
+declare let taxNumber: number;
+declare let taxString: string;
+declare let taxValue: string | number;
+declare let newTaxValue: string | number;
---
+++ dist/types.js
@@ -0,0 +1,50 @@
+"use strict";
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 'Hello';
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
+function calaculateTax(amount) {
+    return amount * 0.16;
+}
+function calculateTaxInclusive(amount) {
+    return `$${(amount * 1.16).toFixed(2)}`;
+}
+let price = 100;
+let taxAmount = calaculateTax(price);
+let halfShare = (taxAmount / 2).toFixed(2);
+console.log(`${12} = ${calaculateTax(12)}`);
+// console.log(`${'Hello'} = ${calaculateTax('Hello')}`);
+// console.log(`${'True'} = ${calaculateTax(true)}`);
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
+console.log(`Tax Inclusive price: ${calculateTaxInclusive(120)}`);
+function newCalculateTax(amount, format) {
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
+}
+let taxNumber = newCalculateTax(100, false);
+let taxString = newCalculateTax(100, true);
+console.log(`Number Value:  ${typeof taxNumber}, ${taxNumber.toFixed(2)}`);
+console.log(`String Value: ${typeof taxString} ${taxString.charAt(0)}`);
+//Using a type guard
+let taxValue = newCalculateTax(100, false);
+if (typeof taxValue === 'number') {
+    console.log(` This Number value: ${taxValue.toFixed(2)}`);
+}
+else if (typeof taxValue === 'string') {
+    console.log(`This String value : ${taxValue.charAt(0)}`);
+}
+let newTaxValue = newCalculateTax(100, false);
+switch (typeof newTaxValue) {
+    case 'number':
+        console.log(`Switch number value : ${newTaxValue.toFixed(5)}`);
+        break;
+    case 'string':
+        console.log(`Switch string value: ${newTaxValue.charAt(3)}`);
+        break;
+}
+// Unknown Type
